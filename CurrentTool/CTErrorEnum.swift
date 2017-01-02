11f6353486d4706785f6fd7ac6902fe4b3869002
gmlangel <guominglong@Mac-mini.local>
2017-01-02 16:16:27 +0800

    优化

--- CurrentTool/CTErrorEnum.swift
+++ CurrentTool/CTErrorEnum.swift
@@ -13,4 +13,6 @@ enum CTErrorEnum:Int {
     case assetsExport_TargetPath_CreateFaild = 1/*AssetsExport时target路径创建失败*/
     case assetsExport_SourcePath_ForeachFaild = 2/*AssetsExport时source路径遍历失败*/
     case assetsExport_CopyErr = 3/*AssetsExport时Copy发生错误*/
+    case pushNotification_Err = 4/*pushNotification的操作系统版本和处理函数不对应*/
+    case pushNotification_NullNotifyKey = 5/*pushNotification创建本地推送的时候缺少notify唯一key*/
 }
